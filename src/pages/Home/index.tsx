253dfd26273db50a958363a646f2738ec7fd39b1
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-12 14:55:54 -0300

    Finished Home page.

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  Separator,
+  StartCountdownButton,
+  TaskDuration,
+  TaskInput,
+} from './styles'
+
+export const Home = () => {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para sua tarefa"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <TaskDuration
+            id="minutesAmount"
+            placeholder="00"
+            type="number"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton type="submit" disabled>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
