09b2e06dafb9ea59ec9200df55eeb6fbf9af6b5d
Eric Edward Kunzel <kunzel.eric@gmail.com>
2023-06-12 11:15:06 -0300

    Configured routes in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
+import { Router } from './components/Router'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
